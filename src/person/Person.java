6db1e26b18ca40337a585a467f3610dac9e6ec2d
Ashutosh Tripathi <ashutosh.tripathi_cs18@gla.c.in>
2020-02-23 15:26:02 +0530

    field created : contacts

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,8 +1,9 @@
 package person;
 
+import java.util.LinkedList;
+
 public class Person {
     private String firstName;
     private String lastName;
-
-
+    private LinkedList<String> contacts;
 }
